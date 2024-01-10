3f3a9349d1a9da72361eccd261625f0a87a6c70a
yy <lingdie.yy@outlook.com>
2024-01-10 15:07:00 +0800

    ci: add init container.

--- scripts/build-cluster-image.sh
+++ scripts/build-cluster-image.sh
@@ -1,1 +1,32 @@
-echo "building cluster image for $1"
+#!/bin/bash
+
+# $1: template file name
+# $2: image registry and repo
+# $3: image tag
+
+echo "Building cluster image for $1"
+
+template_file=$1
+template_name=$(basename "$template_file" .yaml)
+image_name="$2/$template_name:$3"
+
+if [[ ! -f "$template_file" ]]; then
+  echo "Template file $template_file not found"
+  exit 1
+fi
+
+# prepare for build cluster image
+rm -rf build
+mkdir -p build && mkdir -p build/manifests
+
+echo "
+FROM scratch
+
+USER 65532:65532
+
+COPY registry registry
+
+CMD ["echo "uploaded images for template $template_name""]
+" > build/Kubefile
+
+sealos build -f build/Kubefile -t "$image_name"
